d65dbc59a02340b4295828dce89322f93fc218ae
prynetopg777 <topg.official07@gmail.com>
2023-04-15 19:03:46 +0800

    router and header changes

--- src/routes/searchRouter.js
+++ src/routes/searchRouter.js
@@ -2,32 +2,43 @@
 import { Router } from "express";
 // Models
 import User from "../models/UserModel.js";
+const bodyParser = require('body-parser')
 
+const app = express();
 
-const searchRouter = Router();
+app.use(bodyParser.urlencoded({ extended: false}));
 
-searchRouter.get("/search", (req, res) => {
- const input_user = req.body.query;
-
-  console.log(input_user);
-
-  User.findOne({ userName: input_user })
-    .then((result) => {
-      if (result == null) {
-        console.log("No such user found: " + query);
-        res.redirect("/");
-        return;
-      }
-
-      res.redirect('/profile/' + result.userName);
-    })
-    .catch((err) => {
-      console.error("An error occurred: " + err);
-      res.statusCode = 500;
-      res.render("error", {
-        code: 500,
-      });
-    });
+app.post("/search", (req, res) => {
+    const searchInput = req.body.searchInput;
+    console.log(searchInput);
+    res.redirect("/profile/" + searchInput);
 });
+
+app.listen(3000, () => console.log("server listening"));
+
+
+// searchRouter.get("/search", (req, res) => {
+//  const input_user = req.body.query;
+
+//   console.log(input_user);
+
+//   User.findOne({ userName: input_user })
+//     .then((result) => {
+//       if (result == null) {
+//         console.log("No such user found: " + query);
+//         res.redirect("/");
+//         return;
+//       }
+
+//       res.redirect('/profile/' + result.userName);
+//     })
+//     .catch((err) => {
+//       console.error("An error occurred: " + err);
+//       res.statusCode = 500;
+//       res.render("error", {
+//         code: 500,
+//       });
+//     });
+// });
 
 export default searchRouter;
