1660198e6a13297e3b2bdaded0c693d07ea951f4
KnightofSpace <jeanine_marie_ignacio@dlsu.edu.ph>
2023-04-16 17:56:06 +0800

    settings update

--- src/routes/settingsRouter.js
+++ src/routes/settingsRouter.js
@@ -13,42 +13,58 @@ settingsRouter.get("/settings/:userName", (req, res) => {
 });
 
 // Handle form submission from settings view
-settingsRouter.post("/settings", (req, res) => {
+settingsRouter.post("/settings/:userName", (req, res) => {
   const { bio, links } = req.body;
 
   // Update the User model in the database with the new bio and links data
-  const user = {
+  (async () => {
+    try {
+      const user = await User.findOneAndUpdate(
+        {userName: req.params.userName},
+        { bio, links }, // Update the bio and links fields
+        { new: true } // Return the updated user object
+        );
 
-    userName: req.params.userName,
-    // email:    email,
-    // pw:       pw,
-    // avatar:   avatar,
-    // bio:      bio,
-    // tags:     tags,
-    // links:    links,
+        console.log("Username is " + user.userName);
+        res.redirect("/profile/" + user.userName);
+
+    } catch(err){
+        console.error(err);
+    }
+  })();
+
+  //console.log("Username(again) is " + user.userName);
+
+  // const user = {
+
+  //   userName: req.params.userName,
+  //   // email:    email,
+  //   // pw:       pw,
+  //   // avatar:   avatar,
+  //   // bio:      bio,
+  //   // tags:     tags,
+  //   // links:    links,
   
   
-    };
+  //   };
 
-  console.log("Username is" + user.userName);
+  // User.findOneAndUpdate(
 
-  User.findOneAndUpdate(
-
-    { userName: req.user.userName }, // Update the user based on the currently authenticated user
-    { bio, links }, // Update the bio and links fields
-    { new: true } // Return the updated user object
-  )
-    .then((result) => {
-      // Redirect back to the profile view
-      res.redirect("/profile/" + result.userName);
-    })
-    .catch((err) => {
-      console.error("An error occurred: " + err);
-      res.statusCode = 500;
-      res.render("error", {
-        code: 500,
-      });
-    });
+  //   { userName: req.user.userName }, // Update the user based on the currently authenticated user
+  //   { bio, links }, // Update the bio and links fields
+  //   { new: true } // Return the updated user object
+  // )
+  //   .then((result) => {
+  //     // Redirect back to the profile view
+  //     res.redirect("/profile/" + result.userName);
+  //   })
+  //   .catch((err) => {
+  //     console.error("An error occurred: " + err);
+  //     res.statusCode = 500;
+  //     res.render("error", {
+  //       code: 500,
+  //     });
+  //   });
 });
 
 export default settingsRouter;
